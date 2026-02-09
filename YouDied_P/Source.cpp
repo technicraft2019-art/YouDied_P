a6381bc39fe678024ed4fdb0dfb35bdbb1b4d04f
Charles Poole <technicraft2019@gmail.com>
2026-02-08 21:44:47 -0500

    first edit of Source.cpp file

--- YouDied_P/Source.cpp
+++ YouDied_P/Source.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-	std::cout << "You Died!\n";
-
+	// Declares a character variable named myCharacter
+	char myCharacter;
+	// Assigns the value 'y' to myCharacter
+	myCharacter = 'y';
+	// Declares an integer variable named myInt
+	int myInt;
+	// Assigns the value 13 to myInt
+	myInt = 13;
+	// Outputs the value of myInt to the console
+	cout << myCharacter << endl;
+	// Outputs the value of myInt to the console
+	cout << myInt << endl;
+	// Pauses the program until user makes an input
 	system("pause");
 }
